b850ffff50e5e3c3c869c1e816cd8629d3557ea9
Jiya-benurkar <benurkarjiya@gmail.com>
2026-08-05 03:32:35 +0530

    Refactor Page component structure and imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,32 @@ import React, { Suspense } from 'react';
 import CoinOverview from '@/components/home/CoinOverview';
 import TrendingCoins from '@/components/home/TrendingCoins';
 import {
-    CategoriesFallback,
-    CoinOverviewFallback,
-    TrendingCoinsFallback,
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
 } from '@/components/home/fallback';
 import Categories from '@/components/home/Categories';
 
 const Page = async () => {
-    return (
-        <main className="main-container">
-            <section className="home-grid">
-                <Suspense fallback={<CoinOverviewFallback />}>
-                    <CoinOverview />
-                </Suspense>
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-                <Suspense fallback={<TrendingCoinsFallback />}>
-                    <TrendingCoins />
-                </Suspense>
-            </section>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
 
-            <section className="w-full mt-7 space-y-4">
-                <Suspense fallback={<CategoriesFallback />}>
-                    <Categories />
-                </Suspense>
-            </section>
-        </main>
-    );
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
